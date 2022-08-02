559f8cd2e4feb8b4dd5fa141c2ecc48bcd5026d8
gsd <mr.little.cat.ru@gmail.com>
2022-08-02 16:09:44 +0300

    fix usercon

--- src/main/java/mvc/controllers/UserController.java
+++ src/main/java/mvc/controllers/UserController.java
@@ -1,6 +1,5 @@
 package mvc.controllers;
 
-import mvc.dao.UserDAO;
 import mvc.models.User;
 import mvc.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class UserCon {
+public class UserController {
     private UserService service;
 
     @Autowired
